af6ec8140935c011b7107eb58ae4a9c8c27bb963
balajisaiveer <puppalabalaji@gmail.com>
2018-07-10 19:27:22 +0530

    Add files via upload

---
+++ workspace-sts-3.9.5.RELEASE/Myapplication/src/main/java/com/balaji/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.balaji.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.balaji.modal.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+	public User findByUsernameAndPassword(String username,String password);
+}
